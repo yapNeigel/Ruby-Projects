ce284ee20536cdb205aef8ee148f3d7ad3fc69c0
yapNeigel <yap.neigel@gmail.com>
2018-02-07 15:28:55 +0800

    2/7/2018

--- deaf_grandma.rb
+++ deaf_grandma.rb
@@ -1,13 +1,16 @@
 status = false
-
+byeStatus = 0
 while status==false
 	puts "What do you wanna say to Deaf Grandma?"
 	saying = gets.chomp
 
 	if saying == saying.upcase
 		if saying == "BYE"
-			status = true
-			puts "ALRIGHT, SONNY BOY! EAT THOSE COOKIES I MADE YOU! GOOD BYE!"
+			byeStatus+=1
+			if byeStatus==3
+				status = true
+				puts "ALRIGHT, SONNY BOY! EAT THOSE COOKIES I MADE YOU! GOOD BYE!"
+			end
 		else
 			puts "NO, NOT SINCE "+(rand(1930..1950)).to_s
 		end
